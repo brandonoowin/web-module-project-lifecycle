d985262f4970f29d6d46f64fb06976b510cdf771
Brandon Nguyen <brandonxnguyen@gmail.com>
2023-09-15 16:36:24 -0700

    created form component passing in props

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,15 +1,22 @@
 import React from 'react'
 
 export default class Form extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      todoNameInput: 'foo', 
-    }
-  }
   render() {
     return (
       <div>
+          <form onSubmit={this.props.onSubmit}>
+            <input 
+            value={this.props.todoNameInput} 
+            type='text' 
+            placeholder='Type Todo' 
+            onChange={this.props.onChange}/>
+            <input type='submit' />
+          </form>
+          <button 
+          onClick={this.props.toggleDisplayCompleted}
+          >
+            {this.props.displayCompleted ? ' Hide' : ' Show'} Completed
+            </button>
       </div>
     )
   }
